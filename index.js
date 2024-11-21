3dcb0dc9d7043d6fa49de117c956d91bab626531
sanidhya290 <148427917+sanidhya290@users.noreply.github.com>
2024-11-21 18:07:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const cors =require("cors")
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.listen(4000,()=>{
-    console.log("running on port 4000");
-})
+const port = process.env.PORT || 4000;  // Use Render's dynamic port or fallback to 4000
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 const upload = multer({ storage: multer.memoryStorage() });
 
 const filterData = (arr) => {
